7dd241cf9696b2bd8d8bd662da6600d57866bb9d
Kurzhalsgiraffe <max-hantschke@web.de>
2022-12-09 18:05:12 +0100

    Revert "Update databaseaccess.py"
    This reverts commit 3e5fa4e2bcf1564e677fa4b77637e892bf97cf7d.

--- databaseaccess.py
+++ databaseaccess.py
@@ -92,7 +92,6 @@ class Dao:
                 for j in data:
                     if j[0] == i:
                         arr.append(j)
-                        data.remove(j)
                         break
                 else:
                     arr.append(None)
