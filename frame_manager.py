407085602b77d600e38c91df5b274dc14fb31b6f
Kurzhalsgiraffe <max-hantschke@web.de>
2023-07-02 17:43:09 +0200

    moved functions to external class

---
+++ frame_manager.py
@@ -0,0 +1,235 @@
+import json
+import time
+
+from database_access import Dao
+from led_hardware import LEDMatrix
+
+STANDARD_ANIMATION_TIME = 200 # If changed, also change in JS!
+
+class Animation:
+    """ managing animations """
+    def __init__(self, database, led):
+        self.database = database
+        self.led = led
+        self.running = False
+        self.stopped = True
+        self.speed = read_settings("speed")
+
+    def set_speed(self, speed):
+        """ Set the percentage of the animation speed """
+        speed = float(speed)
+        self.speed = speed
+        write_settings("speed", speed)
+
+    def get_animationlist(self, animation_id):
+        animationlist = []
+        data = self.database.get_animationlist_by_id(animation_id)
+        if data:
+            binarys = self.database.load_multiple_binarys(data["imageIDs"])
+
+            for binary, sleep_time in zip(binarys, data["times"]):
+                animationlist.append([binary[1], sleep_time/1000])
+        return animationlist
+
+    def start_animation(self, animation_id):
+        """ Start animation by id """
+        animationlist = self.get_animationlist(animation_id)
+        if animationlist:
+            self.running = True
+            self.stopped = False
+
+            while self.running:
+                for binary, sleep_time in animationlist:
+                    if self.running:
+                        self.led.update_frame(binary)
+                        time.sleep(sleep_time*(1/(self.speed)))
+            self.stopped = True
+
+    def stop(self):
+        """ Stop the animation """
+        while self.stopped is False:
+            self.running = False
+
+class FrameManager:
+    def __init__(self):
+        self.database = Dao("database.sqlite")
+        self.led = LEDMatrix(brightness=read_settings("brightness"))
+        self.animation = Animation(self.database, self.led)
+
+    def apply_color_array(self, color_array):
+        binary = color_array_to_binary(color_array)
+        self.led.update_frame(binary)
+
+    def apply_image_id(self, image_id):
+        binary = self.database.load_single_binary(image_id)
+        self.led.update_frame(binary)
+        write_settings("last_applied_image_id", image_id)
+
+    def load_animation_info_all(self):
+        animation_ids = []
+        animation_names = []
+        data = sorted(self.database.get_all_animations(), key=lambda x: x[0])
+        if data:
+            for i in data:
+                animation_ids.append(i[0])
+                if i[1]=="null":
+                    animation_names.append("Animation "+str(i[0]))
+                else:
+                    animation_names.append(i[1])
+
+            thumbnail_ids = self.database.get_all_animation_thumbnail_ids(animation_ids)
+
+            data = {
+                    "animationIDs": animation_ids,
+                    "animationNames": animation_names,
+                    "thumbnailIDs": thumbnail_ids
+                }
+            return data
+        return None
+    
+    def load_single_frame(self, image_id, pos):
+        skip_offset = read_settings("skip_offset")
+
+        if pos:
+            if pos == "first":
+                image_id = self.database.get_first_image_id()
+            elif pos == "fastbackwards":
+                image_id = self.database.get_ffw_image_id(image_id, skip_offset)
+            elif pos == "prev":
+                image_id = self.database.get_previous_image_id(image_id)
+            elif pos == "next":
+                image_id = self.database.get_next_image_id(image_id)
+            elif pos == "fastforwards":
+                image_id = self.database.get_fbw_image_id(image_id, skip_offset)
+            elif pos == "last":
+                image_id = self.database.get_last_image_id()
+
+        binary = self.database.load_single_binary(image_id)
+        if binary:
+            data = {
+                "colorArray": binary_to_color_array(binary),
+                "imageID": image_id
+            }
+            return data
+        return None
+    
+    def save_color_array(self, color_array):
+        binary = color_array_to_binary(color_array)
+        self.database.save_binary(binary)
+
+    def replace_color_array(self, image_id, color_array):
+        binary = color_array_to_binary(color_array)
+        self.database.replace_binary(image_id,binary)
+
+    def load_multiple_binaries(self, image_ids):
+        data = []
+        if image_ids:
+            binarys = self.database.load_multiple_binarys(image_ids)
+            for i in binarys:
+                if i:
+                    data.append((i[0], binary_to_color_array(bytearray(i[1]))))
+                else:
+                    data.append(None)
+        return data
+
+    def apply_brightness(self, brightness):
+        if read_settings("power") == "on":
+            self.led.update_brightness(int(brightness))
+        write_settings("brightness", int(brightness))
+
+    def apply_power(self, power):
+        if power == "on":
+            brightness = int(read_settings("brightness"))
+            self.led.update_brightness(brightness)
+        elif power == "off":
+            self.led.update_brightness(0)
+
+        write_settings("power", power)
+
+    def create_animation(self, name):
+        self.database.create_animation(name)
+
+    def add_animation_frame(self, animation_id, image_id):
+        last_pos = self.database.get_last_position_by_animation_id(animation_id)
+        if last_pos:
+            next_pos = last_pos + 1
+        else:
+            next_pos = 1
+
+        self.database.add_image_to_animation(animation_id, image_id, next_pos, STANDARD_ANIMATION_TIME)
+
+    def update_time_for_animationframe(self, animation_id, position, time):
+        if position == "all":
+            self.database.update_animation_time_of_all_frames(animation_id, time)
+        else:
+            self.database.update_animation_time_of_single_frame(animation_id, position, time)
+
+    def switch_animation_positions(self, animation_id, source_id, target_id):
+        self.database.switch_animation_positions(animation_id, source_id, target_id)
+
+    def delete_image(self, image_id):
+        self.database.delete_binary(int(image_id))
+
+    def delete_animation(self, animation_id):
+        self.database.delete_animation(animation_id)
+        self.database.remove_all_images_from_animation(animation_id)
+
+    def remove_animation_frame(self, animation_id, position):
+        self.database.remove_image_from_animation(animation_id, position)
+
+    def vacuum_database(self):
+        self.database.vacuum()
+
+    def get_animationlist_by_id(self, animation_id):
+        self.database.get_animationlist_by_id(animation_id)
+
+
+
+
+
+
+
+
+
+# FUNCTIONS
+
+def write_settings(key, value):
+    """
+    Write the key value pair the settings file
+    """
+    with open('settings.json', 'r', encoding="utf-8") as file:
+        settings = json.load(file)
+        settings[key] = value
+
+    with open('settings.json', 'w', encoding="utf-8") as file:
+        json.dump(settings, file)
+
+def read_settings(key):
+    """
+    Read the value to the key from the settings file
+    """
+    with open('settings.json', 'r', encoding="utf-8") as file:
+        settings = json.load(file)
+    if key in settings:
+        return settings[key]
+    return None
+
+def color_array_to_binary(color_array):
+    """
+    Calculate bytearray from color_array
+    """
+    byte_array = bytearray()
+    for color in color_array:
+        byte_array.append(int(color[1:3], 16))
+        byte_array.append(int(color[3:5], 16))
+        byte_array.append(int(color[5:7], 16))
+    return byte_array
+
+def binary_to_color_array(binary):
+    """
+    Calculate color_array from bytearray
+    """
+    color_array = []
+    for i in range(0,768,3):
+        color_array.append(f"#{(binary[i]*16**4+binary[i+1]*16**2+binary[i+2]):06x}")
+    return color_array
--- server.py
+++ server.py
@@ -1,61 +1,12 @@
 """Flask Server: This is the main program"""
-import time
-import utils
+import frame_manager
+
 from flask import Flask, request, jsonify, render_template
 from threading import Thread
 from waitress import serve
 
-from databaseaccess import Dao
-from led_hardware import LEDMatrix
-
-STANDARD_ANIMATION_TIME = 200 # If changed, also change in JS!
-
-class Animation:
-    """ managing animations """
-    def __init__(self):
-        self.running = False
-        self.stopped = True
-        self.speed = utils.read_settings("speed")
-
-    def set_speed(self, speed):
-        """ Set the percentage of the animation speed """
-        speed = float(speed)
-        self.speed = speed
-        utils.write_settings("speed", speed)
-
-    def get_animationlist(self, animation_id):
-        animationlist = []
-        data = database.get_animationlist_by_id(animation_id)
-        if data:
-            binarys = database.load_multiple_binarys(data["imageIDs"])
-
-            for binary, sleep_time in zip(binarys, data["times"]):
-                animationlist.append([binary[1], sleep_time/1000])
-        return animationlist
-
-    def start_animation(self, animation_id):
-        """ Start animation by id """
-        animationlist = self.get_animationlist(animation_id)
-        if animationlist:
-            self.running = True
-            self.stopped = False
-
-            while self.running:
-                for binary, sleep_time in animationlist:
-                    if self.running:
-                        led.update_frame(binary)
-                        time.sleep(sleep_time*(1/(self.speed)))
-            self.stopped = True
-
-    def stop(self):
-        """ Stop the animation """
-        while self.stopped is False:
-            self.running = False
-
 app = Flask(__name__)
-led = LEDMatrix(brightness=utils.read_settings("brightness"))
-animation = Animation()
-database = Dao("database.sqlite")
+manager = frame_manager.FrameManager()
 
 ## ----- GET ----- ##
 
@@ -82,7 +33,7 @@ def animation_editor_page():
 @app.route("/animation/load/<animation_id>")
 def animation_load(animation_id):
     """ Load all informations of this animation """
-    data = database.get_animationlist_by_id(animation_id)
+    data = manager.get_animationlist_by_id(animation_id)
     if data:
         return jsonify(data)
     return {},400
@@ -90,77 +41,41 @@ def animation_load(animation_id):
 @app.route("/animation/load/all")
 def animation_load_all():
     """ Load all animation_ids, animation_names and the image_id of their first image """
-    animation_ids = []
-    animation_names = []
-    data = sorted(database.get_all_animations(), key=lambda x: x[0])
+    data = manager.load_animation_info_all()
     if data:
-        for i in data:
-            animation_ids.append(i[0])
-            if i[1]=="null":
-                animation_names.append("Animation "+str(i[0]))
-            else:
-                animation_names.append(i[1])
-
-        thumbnail_ids = database.get_all_animation_thumbnail_ids(animation_ids)
-
-        data = {
-                "animationIDs": animation_ids,
-                "animationNames": animation_names,
-                "thumbnailIDs": thumbnail_ids
-            }
         return jsonify(data)
     return {},400
 
 @app.route("/image/load/single")
 def image_load_single():
     """  Calculate image_id based on current image_id and pos, and return image and new image_id """
-    image_id = request.args.get('image_id', type = int)
+    current_image_id = request.args.get('image_id', type = int)
     pos = request.args.get('pos', type = str)
 
-    skip_offset = utils.read_settings("skip_offset")
-
-    if pos:
-        if pos == "first":
-            image_id = database.get_first_image_id()
-        elif pos == "fastbackwards":
-            image_id = database.get_ffw_image_id(image_id,skip_offset)
-        elif pos == "prev":
-            image_id = database.get_previous_image_id(image_id)
-        elif pos == "next":
-            image_id = database.get_next_image_id(image_id)
-        elif pos == "fastforwards":
-            image_id = database.get_fbw_image_id(image_id,skip_offset)
-        elif pos == "last":
-            image_id = database.get_last_image_id()
-
-    binary = database.load_single_binary(image_id)
-    if binary:
-        data = {
-            "colorArray": utils.binary_to_color_array(binary),
-            "imageID": image_id
-        }
+    data = manager.load_single_frame(current_image_id, pos)
+    if data:
         return jsonify(data)
     return {},400
 
 @app.route("/brightness/load")
 def brightness_load():
     """ Load current LED brightness value """
-    return str(utils.read_settings("brightness"))
+    return str(frame_manager.read_settings("brightness"))
 
 @app.route("/language/load")
 def language_load():
     """ Load current language """
-    return str(utils.read_settings("language"))
+    return str(frame_manager.read_settings("language"))
 
 @app.route("/speed/load")
 def speed_load():
     """ Load current animation speed value """
-    return str(utils.read_settings("speed"))
+    return str(frame_manager.read_settings("speed"))
 
 @app.route("/power/load")
 def power_load():
     """ Load current power status """
-    return str(utils.read_settings("power"))
+    return str(frame_manager.read_settings("power"))
 
 ## ----- POST ----- ##
 
@@ -168,38 +83,30 @@ def power_load():
 def apply_color_array():
     """ Apply color array from json request to the frame """
     color_array = request.json
-    binary = utils.color_array_to_binary(color_array)
-    led.update_frame(binary)
+    manager.apply_color_array(color_array)
     return {}
 
 @app.route("/image/apply_id", methods=["POST"])
 def apply_id():
     """ Apply image by id to the frame """
     image_id = request.args.get('image_id', type = int)
-
-    binary = database.load_single_binary(image_id)
-    led.update_frame(binary)
-    utils.write_settings("last_applied_image_id", image_id)
+    manager.apply_image_id(image_id)
     return {}
 
 @app.route("/image/save", methods=["POST"])
 def save():
-    """ Save a new image to the database """
+    """ Save a new image """
     color_array = request.json
-    binary = utils.color_array_to_binary(color_array)
-    database.save_binary(binary)
+    manager.save_color_array(color_array)
     return {}
 
 @app.route("/image/replace", methods=["POST"])
 def replace():
     """ Replace an image by a new image """
     image_id = request.args.get('image_id', type = int)
-
     color_array = request.json
-    binary = utils.color_array_to_binary(color_array)
-
-    if image_id:
-        database.replace_binary(image_id,binary)
+    if image_id and color_array:            # TODO: Dont know if its nessesary to check for color_array too
+        manager.replace_color_array(image_id, color_array)
         return {}
     return {},400
 
@@ -207,79 +114,59 @@ def replace():
 def image_load_multiple():
     """ Load multiple binarys by image_id """
     image_ids = request.json
-    if image_ids:
-        binarys = database.load_multiple_binarys(image_ids)
-        data = []
-        for i in binarys:
-            if i:
-                data.append((i[0], utils.binary_to_color_array(bytearray(i[1]))))
-            else:
-                data.append(None)
+    data = manager.load_multiple_binaries(image_ids)
+    if data:
         return jsonify(data)
     return {},400
 
 @app.route("/brightness/apply/<brightness>", methods=["POST"])
 def brightness_apply(brightness):
     """ Apply the brightness value to the LED strip """
-    utils.write_settings("brightness", int(brightness))
-    if utils.read_settings("power") == "on":
-        led.update_brightness(int(brightness))
+    manager.apply_brightness(brightness)
     return {}
 
 @app.route("/language/apply/<language>", methods=["POST"])
 def language_apply(language):
     """ Apply language to the UI """
-    utils.write_settings("language", language)
+    frame_manager.write_settings("language", language)
     return {}
 
 @app.route("/speed/apply/<speed>", methods=["POST"])
 def speed_apply(speed):
     """ Apply a speed value to the animation """
-    animation.set_speed(float(speed))
+    manager.animation.set_speed(float(speed))
     return {}
 
 @app.route("/power/apply/<power>", methods=["POST"])
 def power_apply(power):
     """ Apply power status to the LED-Matrix """
-    if power == "on":
-        brightness = int(utils.read_settings("brightness"))
-        led.update_brightness(brightness)
-    elif power == "off":
-        led.update_brightness(0)
-
-    utils.write_settings("power", power)
+    manager.apply_power(power)
     return {}
 
 @app.route("/animation/start/<animation_id>", methods=["POST"])
 def animation_start(animation_id):
     """ Start a animation, stop the currently running if nessessary """
-    animation.stop()
-    t = Thread(target=animation.start_animation, args=(animation_id))
+    manager.animation.stop()
+    t = Thread(target=manager.animation.start_animation, args=(animation_id))
     t.start()
     return {}
 
 @app.route("/animation/stop", methods=["POST"])
 def animation_stop():
     """ Stop the currently running animation """
-    animation.stop()
+    manager.animation.stop()
     return {}
 
 @app.route("/animation/create/<name>", methods=["POST"])
 def animation_create(name):
     """ Create a new animation """
-    database.create_animation(name)
+    manager.create_animation(name)
     return {}
 
-@app.route("/animation/frame/add/<animation_id>/<image_id>", methods=["POST"])
+@app.route("/animation/frame/add/<animation_id>/<image_id>", methods=["POST"]) # TODO: add response stuff here
 def animation_frame_add(animation_id, image_id):
     """ Add a frame to an animation """
-    last_pos = database.get_last_position_by_animation_id(animation_id)
-    if last_pos:
-        next_pos = last_pos + 1
-    else:
-        next_pos = 1
-
-    database.add_image_to_animation(animation_id, image_id, next_pos, STANDARD_ANIMATION_TIME)
+    manager.add_animation_frame(animation_id, image_id)
     return {}
 
 @app.route("/animation/frame/updatetime", methods=["POST"])
@@ -287,60 +174,47 @@ def animation_frame_updatetime():
     """ Update the sleep time of an animation frame """
     animation_id = request.args.get('animation_id', type = int)
     position = request.args.get('position', type = str)
-    sleep_time = request.args.get('time', type = int)
-
-    if position == "all":
-        database.update_animation_time_of_all_frames(animation_id, sleep_time)
-    else:
-        database.update_animation_time_of_single_frame(animation_id, position, sleep_time)
+    time = request.args.get('time', type = int)
+    manager.update_time_for_animationframe(animation_id, position, time)
     return {}
 
 @app.route("/animation/frame/switchpositions", methods=["POST"])
 def animation_frame_switchpositions():
-    """ Switch positions of two images in an animation """
+    """ switch positions of two images in an animation """
     animation_id = request.args.get('animation_id', type = int)
     source_id = request.args.get('source_id', type = int)
     target_id = request.args.get('target_id', type = int)
-
-    database.switch_animation_positions(animation_id, source_id, target_id)
+    manager.switch_animation_positions(animation_id, source_id, target_id)
     return {}
 
 ## ----- DELETE ----- ##
 
-@app.route("/delete/<image_id>", methods=["DELETE"])
+@app.route("/delete/<image_id>", methods=["DELETE"])  # TODO: RENAME
 def delete(image_id):
-    """ Delete image from database """
-    database.delete_binary(int(image_id))
+    """ delete image by id"""
+    manager.delete_image(image_id)
     return {}
 
 @app.route("/animation/delete/<animation_id>", methods=["DELETE"])
 def animation_delete(animation_id):
-    """ Delete animation and remove all images from the intermediate table """
-    database.delete_animation(animation_id)
-    database.remove_all_images_from_animation(animation_id)
+    """ delete animation and remove all images from the intermediate table """
+    manager.delete_animation(animation_id)
     return {}
 
 @app.route("/animation/frame/remove", methods=["DELETE"])
 def animation_frame_remove():
-    """ Remove frame from animation """
+    """ remove frame from animation """
     animation_id = request.args.get('animation_id', type = int)
     position = request.args.get('pos', type = int)
-
-    database.remove_image_from_animation(animation_id, position)
+    manager.remove_animation_frame(animation_id, position)
     return {}
 
-## ----- FUNCTIONS ----- ##
-
-def startup_image(image_id):
-    """ Show image on startup """
-    binary = database.load_single_binary(image_id)
-    led.update_frame(binary)
-
 if __name__ == "__main__":
-    startup_image(utils.read_settings("last_applied_image_id"))
+    last_applied_image_id = frame_manager.read_settings("last_applied_image_id")
+    manager.apply_image_id(last_applied_image_id)
 
-    if utils.read_settings("auto vacuum") == True:
-        database.vacuum()
+    if frame_manager.read_settings("auto vacuum") == True:
+        manager.vacuum_database()
 
     if __debug__:
         app.run(debug=True, host="0.0.0.0")
